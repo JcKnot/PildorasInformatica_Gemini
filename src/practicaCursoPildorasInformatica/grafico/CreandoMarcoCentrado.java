6d258dd78c3c149b4f39ed54b03f50ecb349391e
Jose Castillo <107131974+JcKnot@users.noreply.github.com>
2026-03-25 21:19:41 -0400

    Graficos desde casa

---
+++ src/practicaCursoPildorasInformatica/grafico/CreandoMarcoCentrado.java
@@ -0,0 +1,35 @@
+package practicaCursoPildorasInformatica.grafico;
+
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jose Castillo
+ *
+ */
+public class CreandoMarcoCentrado {
+    public static void main(String[] args) {
+        MarcoCentrado marco = new MarcoCentrado();
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setVisible(true);
+    }
+}
+
+class MarcoCentrado extends JFrame{
+    
+    public MarcoCentrado(){
+        Toolkit tool = Toolkit.getDefaultToolkit();
+        Dimension screen = tool.getScreenSize();
+        int alturaScreen = screen.height;
+        int anchoScreen = screen.width;
+        setSize(anchoScreen / 2, alturaScreen / 2);
+        setLocation(anchoScreen / 4, alturaScreen / 4);
+        setTitle("MarcoCentrado");
+//        Image icono = tool.getImage("src/practicaCursoPildorasInformatica/grafico/icono.png")
+//        setIconImage(icono);
+
+    }
+}
---
+++ src/practicaCursoPildorasInformatica/grafico/CreandoMarcos.java
@@ -0,0 +1,31 @@
+package practicaCursoPildorasInformatica.grafico;
+
+import java.awt.Frame;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jose Castillo
+ *
+ */
+public class CreandoMarcos {
+    public static void main(String[] args) {
+        
+        miMarco marco1 = new miMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
+
+class miMarco extends JFrame{
+    
+    public miMarco(){
+        setSize(500, 300);
+        setLocation(500,300);
+        setBounds(500,300,550,250);
+        setTitle("Mi ventana");
+//        setResizable(false);
+//        setExtendedState(Frame.MAXIMIZED_BOTH);
+    }
+}
