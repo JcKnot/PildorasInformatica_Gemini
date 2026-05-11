883804f9288ad73fcc52c947917fd4bc23ae628d
JcKnot <jcknot@gmail.com>
2026-05-11 16:23:48 -0400

    checkbox

---
+++ src/practicaCursoPildorasInformatica/grafico/PruebaCheck.java
@@ -0,0 +1,81 @@
+
+package practicaCursoPildorasInformatica.grafico;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Jose Castillo
+ *
+ */
+public class PruebaCheck {
+    
+    public static void main(String[] args) {
+        
+        MarcoCheck miMarco = new MarcoCheck();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
+
+class MarcoCheck extends JFrame{
+    
+    public MarcoCheck(){
+        setBounds(550, 300, 550, 350);
+        setVisible(true);
+        LaminaCheck miLamina = new LaminaCheck();
+        add(miLamina);
+        
+    }
+}
+
+class LaminaCheck extends JPanel{
+    private JLabel texto;
+    private JCheckBox check1, check2;
+    
+    public LaminaCheck(){
+        setLayout(new BorderLayout());
+        Font miLetra = new Font("Serif", Font.PLAIN, 24);
+        texto = new JLabel("En un lugar de la mancha...");
+        texto.setFont(miLetra);
+        
+        JPanel laminaTexto = new JPanel();
+        laminaTexto.add(texto);
+        
+        add(laminaTexto, BorderLayout.CENTER);
+        
+        check1 = new JCheckBox("Negrita");
+        check2 = new JCheckBox("Cursiva");
+        
+        check1.addActionListener(new ManejaChecks());
+        check2.addActionListener(new ManejaChecks());
+        
+        JPanel laminaChecks = new JPanel();
+        laminaChecks.add(check1);
+        laminaChecks.add(check2);
+        
+        add(laminaChecks, BorderLayout.SOUTH);
+    }
+    
+    private class ManejaChecks implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int tipo = 0;
+            
+            if(check1.isSelected()) tipo += Font.BOLD;
+            
+            if(check2.isSelected()) tipo += Font.ITALIC;
+            
+            texto.setFont(new Font("Serif", tipo, 24));
+        }
+        
+    }
+}
